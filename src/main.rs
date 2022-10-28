6104b793e1528ade2846c82ff9f476c41d100eab
Dominik Spitzli <dominik@spitzli.dev>
2022-10-28 09:52:51 +0200

    how to publish all files on server!

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ mod website_handler;
 fn main() {
     let defaul_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
     let public_path = env::var("PUBLIC_PATH").unwrap_or(defaul_path);
-    println!("Public Path: {}", public_path);
     let server = Server::new("127.0.0.1:8081".to_string());
     server.run(WebsiteHandler::new(public_path))
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,7 +1,6 @@
-use crate::http::StatusCode;
-
-use super::http::{Method, Request, Response};
+use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
+use std::fs;
 
 pub struct WebsiteHandler {
     public_path: String,
@@ -11,15 +10,23 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, Some("404 Not Found!".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::NotFound, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, Some("404 Not Found!".to_string())),
+                },
             },
             _ => Response::new(StatusCode::NotFound, None),
         }
